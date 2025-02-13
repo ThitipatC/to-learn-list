2ec0d120748734c5acb3ad59999c356d55f932e5
ThitipatC <thitipatchaiyakul@gmail.com>
2025-02-14 00:16:13 +0800

    restructure into a folder

---
+++ frontend/components/TopicLists.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import SubTopicProps  from "./SubTopic.vue"
+import {Status} from "./Subject.vue"
+export default interface TopicListProps {
+  topic: string
+  status: Status
+  subtopics: SubTopicProps[]
+}
+const props = defineProps<TopicListProps>()
+</script>
+
+<template>
+  <div class="flex justify-between items-center p-3 border rounded-md bg-gray-50">
+    <div class="flex items-center space-x-3">
+      <div class="p-2 bg-blue-100 text-blue-600 rounded-full">
+        <Icon :name="iconName" size="1.5em" />
+      </div>
+      <div>
+        <p class="text-gray-800 font-medium">{{ subject }}</p>
+      </div>
+    </div>
+    <span :class="status === Status.InProgress ? 'text-green-500' : 'text-red-500'">{{status}}</span>
+  </div>
+</template>
---
+++ frontend/pages/subjects/[subject].vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+const subject = ref()
+
+// onMounted(() => {
+//   if (!localStorage.getItem("authToken")) {
+//     router.push('/');
+//   }
+//   fetch('/your-api-endpoint') // Replace with your actual API endpoint
+//     .then(res => res.json())
+//     .then(data => {
+//       subjects.value = data.data; // Adjust based on your API response structure
+//     })
+// });
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen bg-gray-100">
+    <div class="w-3/4 max-w-4xl bg-white shadow-xl rounded-xl p-6">
+    <h1> My {{subject}} Learning Journey</h1>
+      <div class="h-10 w-10 rounded-full animate-spin" v-if="subjects.length === 0"/>
+      <div v-else>
+        <p v-for="(subject, index) in subjects" :key="index">{{ subject }}</p>
+      </div>
+    </div>
+  </div>
+</template>
