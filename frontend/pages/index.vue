24be22f5515608950d16bb7a913e921ed02ae1ae
ThitipatC <thitipatchaiyakul@gmail.com>
2025-02-15 18:01:20 +0800

    edit: pages to display and use the right component

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref} from 'vue'
-import toLearnItem from '~/components/Subject.vue'
+
 
     const topic = ref<string>("")
 </script>
--- frontend/pages/subjects/[subject].vue
+++ frontend/pages/subjects/[subject].vue
@@ -1,8 +1,11 @@
-<script setup>
+<script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
-
-const subject = ref()
+const route = useRoute();
+console.log(`route params are ${route.params}`)
+const subject = route.params.subject
+// fetch for corresponding topics and subtopics
+const topics = ref([{topic_name : "react1", subtopics : [{subtopic_name: "s1", refs : ["s1-1.com", "s1-2.com"]}]},{topic_name : "react2", subtopics : [{subtopic_name: "s2", refs : ["s2-1.com", "s2-2.com"]}]}])
 
 // onMounted(() => {
 //   if (!localStorage.getItem("authToken")) {
@@ -19,10 +22,10 @@ const subject = ref()
 <template>
   <div class="flex justify-center items-center min-h-screen bg-gray-100">
     <div class="w-3/4 max-w-4xl bg-white shadow-xl rounded-xl p-6">
-    <h1> My {{subject}} Learning Journey</h1>
-      <div class="h-10 w-10 rounded-full animate-spin" v-if="subjects.length === 0"/>
+    <h1 class="py-5"> My {{subject}} Learning Journey</h1>
+      <div class="h-10 w-10 rounded-full animate-spin" v-if="topics.length === 0"/>
       <div v-else>
-        <p v-for="(subject, index) in subjects" :key="index">{{ subject }}</p>
+        <TopicLists v-for="(topic, index) in topics" :key = "index" :topic_name= "topic.topic_name" :subtopics ="topic.subtopics"/>
       </div>
     </div>
   </div>
