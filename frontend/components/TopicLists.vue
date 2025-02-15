0ebaf1dc01c513701a6fbc441ce2232040e9387c
ThitipatC <thitipatchaiyakul@gmail.com>
2025-02-15 18:00:42 +0800

    edit: edit props to make sure that the type matches with the responses from backend

--- frontend/components/TopicLists.vue
+++ frontend/components/TopicLists.vue
@@ -1,24 +1,26 @@
 <script setup lang="ts">
-import SubTopicProps  from "./SubTopic.vue"
-import {Status} from "./Subject.vue"
-export default interface TopicListProps {
-  topic: string
-  status: Status
+  import {SubTopicProps} from "./SubTopic.vue"
+  import { ref} from 'vue'
+  interface TopicListProps {
+  topic_name: string
   subtopics: SubTopicProps[]
 }
+const isClicked = ref<boolean>(false)
 const props = defineProps<TopicListProps>()
 </script>
 
 <template>
-  <div class="flex justify-between items-center p-3 border rounded-md bg-gray-50">
-    <div class="flex items-center space-x-3">
-      <div class="p-2 bg-blue-100 text-blue-600 rounded-full">
-        <Icon :name="iconName" size="1.5em" />
-      </div>
-      <div>
-        <p class="text-gray-800 font-medium">{{ subject }}</p>
-      </div>
-    </div>
-    <span :class="status === Status.InProgress ? 'text-green-500' : 'text-red-500'">{{status}}</span>
+<div>
+<button class="flex w-full justify-between items-center p-3 border rounded-md bg-gray-50 border m-1" @click="isClicked = !isClicked"> 
+                {{topic_name}} 
+  </button>
+  <div v-if="isClicked">
+        <ul>
+        <li class="mx-12 list-disc" v-for="(subtopic, index) in subtopics" :key="index">
+            <SubTopic :subtopic_name="subtopic.subtopic_name" :refs = "subtopic.refs"/>
+        </li>
+        </ul>        
   </div>
+</div>
+
 </template>
